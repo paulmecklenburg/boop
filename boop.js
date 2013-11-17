533d573ffbb4d2c07311e76f65cb18ecbeaecfe7
Paul Mecklenburg <paul@none>
2013-11-17 13:56:13 -0500

    add analytics tracking

--- boop.js
+++ boop.js
@@ -152,12 +152,24 @@ var redraw = function(solution) {
     drawCell(robot, "#00B030");
 }
 
+// analytics tracking
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-2808178-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var reset = function() {
     showPath = false;
     do {
         resetPositions();
     } while (!trySolve());
     redraw();
+    _gaq.push(['_trackEvent', 'Boop', 'NewGame']);
 }
 
 reset();
